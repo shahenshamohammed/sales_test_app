9e6483041fac9bd9f310995629326aed8dfe647e
shahenshamohammed <shahenshamohammedam@gmail.com>
2025-09-27 14:57:49 +0400

    cleaned up

--- test/unit/math_utils.dart
+++ test/unit/math_utils.dart
@@ -1,16 +1,2 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:sales_test_app/main.dart';
 
-void main() {
-  test('adds two numbers', () {
-    // Arrange (input data)
-    int a = 2;
-    int b = 3;
 
-    // Act (call the function)
-    int result = add(a, b);
-
-    // Assert (check result)
-    expect(result, 5);
-  });
-}
